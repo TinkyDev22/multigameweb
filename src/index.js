54863464280ac17ba8d61542b248423d3a80d2d9
Martin Borrini <juanmartinborrini758@gmail.com>
2024-01-03 23:26:06 -0300

    Modificación del JS para reducción y optimización del código. Arreglo de HTML para valor que no figuraba en la selección de ataques. Agregado de clases en JS.

--- src/index.js
+++ src/index.js
@@ -1,66 +1,117 @@
-let playerAttack;
-let enemyAttack;
-let playerLives = 3;
-let enemyLives = 3;
-// Selección de botones desde HTML.
-function initGame() {
-    // Ocultar sección de ataque.
-    let sectionSelectAttack = document.getElementById('select-attack');
-    sectionSelectAttack.style.display = 'none';
-    // Ocultar sección reiniciar.
-    let sectionRestart = document.getElementById('restart');
-    sectionRestart.style.display = 'none';
-    // button-pet
-    let buttonPetPlayer = document.getElementById('button-pet');
-    buttonPetPlayer.addEventListener('click', selectPetPlayer);
-    // button-fire
-    let buttonFire = document.getElementById('button-fire');
-    buttonFire.addEventListener('click', FireAttack);
-    // button-water
-    let buttonWater = document.getElementById('button-water');
-    buttonWater.addEventListener('click', WaterAttack);
-    // button-earth
-    let buttonEarth = document.getElementById('button-earth');
-    buttonEarth.addEventListener('click', EarthAttack);
-    // button-restart
-    let restartButton = document.getElementById('button-restart');
-    restartButton.addEventListener('click', restartGame);
+/* ---- Selección de botones desde HTML ---- */
+
+const sectionSelectPet = document.getElementById('select-pet')
+const sectionSelectAttack = document.getElementById('select-attack')
+const buttonPetPlayer = document.getElementById('button-pet')
+const inputHipodoge = document.getElementById('hipodoge')
+const inputCapipepo = document.getElementById('capipepo')
+const inputRatigueya = document.getElementById('ratigueya')
+const buttonFire = document.getElementById('button-fire')
+const buttonWater = document.getElementById('button-water')
+const buttonEarth = document.getElementById('button-earth')
+const restartButton = document.getElementById('button-restart')
+const spanNamePetPlayer = document.getElementById('name-pet-player')
+const spanNamePetEnemy = document.getElementById('name-pet-enemy')
+const spanLivesPetPlayer = document.getElementById('lives-pet-player')
+const spanLivesPetEnemy = document.getElementById('lives-pet-enemy')
+const scoreSection = document.getElementById('scores')
+const playerAttacks = document.getElementById('player-attacks')
+const enemyAttacks = document.getElementById('enemy-attacks')
+const sectionRestart = document.getElementById('restart')
+
+/* ---- Variables globales ---- */
+
+// Mokepones
+let mokepones = []
+
+// Ataques
+let playerAttack
+let enemyAttack
+
+// Vidas
+let playerLives = 3
+let enemyLives = 3
+
+/* ---- Clases ---- */
+
+// Clase principal de mokepones
+class Mokepon {
+    constructor(name, picture, lives) {
+        this.name = name
+        this.picture = picture
+        this.lives = lives
+        this.attacks = []
+    }
 }
-// Valores aleatorios.
+
+// Creación de los mokepones
+let hipodoge = new Mokepon('Hipodoge','./assets/mokepons_mokepon_hipodoge_attack.png', 5)
+let capipepo = new Mokepon('Capipepo', './assets/mokepons_mokepon_capipepo_attack.png', 5)
+let ratigueya = new Mokepon('Ratigueya', './assets/mokepons_mokepon_ratigueya_attack.png', 5)
+
+// Añadido de mokepones en el array
+mokepones.push(hipodoge, capipepo, ratigueya)
+
+// Añadido de ataques a los mokepones
+hipodoge.attacks.push(
+    { id: 'button-water', name: 'Agua' },
+    { id: 'button-water', name: 'Agua' },
+    { id: 'button-water', name: 'Agua' },
+    { id: 'button-earth', name: 'Tierra'},
+    { id: 'button-fire', name: 'Fuego' },
+)
+capipepo.attacks.push(
+    { id: 'button-earth', name: 'Tierra'},
+    { id: 'button-earth', name: 'Tierra'},
+    { id: 'button-earth', name: 'Tierra'},
+    { id: 'button-water', name: 'Agua' },
+    { id: 'button-fire', name: 'Fuego' },
+)
+ratigueya.attacks.push(
+    { id: 'button-fire', name: 'Fuego' },
+    { id: 'button-fire', name: 'Fuego' },
+    { id: 'button-fire', name: 'Fuego' },
+    { id: 'button-earth', name: 'Tierra'},
+    { id: 'button-water', name: 'Agua' },
+)
+
+/* ---- Funciones auxiliares ---- */
+
+// Valores aleatorios
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
-// Selección aleatoria de mascota jugador.
+
+/* ---- Funciones del juego ---- */
+
+// Ejecución de funciones de inicio del juego
+function initGame() {    
+    // Ocultación de secciones de selección de ataque y reinicio de juego
+    sectionSelectAttack.style.display = 'none'
+    sectionRestart.style.display = 'none'
+    buttonPetPlayer.addEventListener('click', selectPetPlayer)
+    buttonFire.addEventListener('click', FireAttack)
+    buttonWater.addEventListener('click', WaterAttack)
+    buttonEarth.addEventListener('click', EarthAttack)
+    restartButton.addEventListener('click', restartGame)
+}
+
+// Selección aleatoria de mascota jugador
 function selectPetPlayer() {
-    // Aparición de los elementos en pantalla.
-    let sectionSelectAttack = document.getElementById('select-attack');
+    // Aparición de los elementos en pantalla
     sectionSelectAttack.style.display = 'flex';
     // Ocultación de elección de mascotas
-    let sectionSelectPet = document.getElementById('select-pet');
     sectionSelectPet.style.display = 'none';
-    // variables de validación para selección de mascotas.
-    let inputHipodoge = document.getElementById('hipodoge');
-    let inputCapipepo = document.getElementById('capipepo');
-    let inputRatigueya = document.getElementById('ratigueya');
-    // Selección del nombre de la mascota del jugador.
-    let spanNamePetPlayer = document.getElementById('name-pet-player');
-    // Selección de la mascota del jugador.
+    // Selección de la mascota del jugador
     if (inputHipodoge.checked) {
-        // HIpodoge
-        // alert('Seleccionaste a Hipodoge');
         spanNamePetPlayer.innerHTML = 'Hipodoge';
     } else if (inputCapipepo.checked) {
-        // Capipepo
-        // alert('Seleccionaste a Capipepo');
         spanNamePetPlayer.innerHTML = 'Capipepo';
     } else if (inputRatigueya.checked) {
-        // Ratigueya
-        // alert('Seleccionaste a Ratigueya');
         spanNamePetPlayer.innerHTML = 'Ratigueya';
     } else {
         alert('Selecciona una Mascota');
     }
-    // Ejecución de la variable de las mascotas enemigas.
     selectPetEnemy();
 }
 // Selección aleatoria de mascota enemiga.
@@ -68,15 +119,11 @@ function selectPetEnemy() {
     // Valor aleatorio para el valor del ataque enemigo.
     let randomEnemy = randomNumber(1,3);
     // Selección de la mascota del enemigo.
-    let spanNamePetEnemy = document.getElementById('name-pet-enemy');
     if (randomEnemy == 1) {
-        // Hipodoge
         spanNamePetEnemy.innerHTML = 'Hipodoge';
     } else if (randomEnemy == 2) {
-        // Capipepo
         spanNamePetEnemy.innerHTML = 'Capipepo';
     } else {
-        // Ratigueya
         spanNamePetEnemy.innerHTML = 'Ratigueya';
     }
 }
@@ -109,9 +156,6 @@ function randomEnemyAttack() {
 }
 // Resultado de los combates entre mascotas.
 function combat() {
-    let spanLivesPetPlayer = document.getElementById('lives-pet-player');
-    let spanLivesPetEnemy = document.getElementById('lives-pet-enemy');
-
     if (playerAttack == enemyAttack) {
         createMessage('Empate');
     } else if (playerAttack == 'Fuego' && enemyAttack == 'Tierra') {
@@ -144,10 +188,6 @@ function lives() {
 }
 // Mensaje de elecciones de jugadores.
 function createMessage(combatResult) {
-    let scoreSection = document.getElementById('scores');
-    let playerAttacks = document.getElementById('player-attacks');
-    let enemyAttacks = document.getElementById('enemy-attacks');
-
     scoreSection.innerHTML = combatResult;
 
     let playerNotification = document.createElement('p');
@@ -160,19 +200,12 @@ function createMessage(combatResult) {
 }
 // Mensaje de resultado final
 function createFinalMessage(finalResult) {
-    let messageSection = document.getElementById('scores');
-    messageSection.innerHTML = finalResult;
-
+    scoreSection.innerHTML = finalResult;
     // Deshabilitación de botones del juego
-    let buttonFire = document.getElementById('button-fire');
     buttonFire.disabled = true;
-    let buttonWater = document.getElementById('button-water');
     buttonWater.disabled = true;
-    let buttonEarth = document.getElementById('button-earth');
     buttonEarth.disabled = true;
-
     // Aparición de botón reiniciar
-    let sectionRestart = document.getElementById('restart');
     sectionRestart.style.display = 'flex';
 }
 // Reiniciar juego
