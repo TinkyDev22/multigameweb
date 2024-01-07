1f3a64a809a1a521958ae5655d343eaf568b721c
Martin Borrini <juanmartinborrini758@gmail.com>
2024-01-07 20:37:52 -0300

    Se agregan creación de botones automáticos. Se agrega elección de botones del elemigo de forma aleatoria. Se reestructura la parte principal del index.js Se modifican variables globales generales

--- src/index.js
+++ src/index.js
@@ -1,35 +1,65 @@
 /* ---- Selección de botones desde HTML ---- */
 
+// Botones y vistas principales
+
+// Vista principal de seleccion de mascotas.
 const sectionSelectPet = document.getElementById('select-pet')
-const sectionSelectAttack = document.getElementById('select-attack')
-const buttonPetPlayer = document.getElementById('button-pet')
-const restartButton = document.getElementById('button-restart')
-const spanNamePetPlayer = document.getElementById('name-pet-player')
-const spanNamePetEnemy = document.getElementById('name-pet-enemy')
-const spanLivesPetPlayer = document.getElementById('lives-pet-player')
-const spanLivesPetEnemy = document.getElementById('lives-pet-enemy')
-const scoreSection = document.getElementById('scores')
-const playerAttacks = document.getElementById('player-attacks')
-const enemyAttacks = document.getElementById('enemy-attacks')
-const sectionRestart = document.getElementById('restart')
+// Sección con los botones de selección de mascotas
 const cardsContainer = document.getElementById('cards-container')
+// Botón para confirmar la selección de mascotas una vez clickeada alguna.
+const buttonPetPlayer = document.getElementById('button-pet')
+
+// Pestaña de detalle en el área de combate.
+
+// Vista principal de seleccion de ataques.
+const sectionSelectAttack = document.getElementById('select-attack')
+// Sección que incluye el botón de reinicio del juego.
+const sectionRestart = document.getElementById('restart')
+// Botón de reinicio del juego una vez finalizada la batalla.
+const restartButton = document.getElementById('button-restart')
+
+// Botones de elementos de los ataques.
 const attacksContainer = document.getElementById('attacks-container')
+
+// Span en el detalle del combate con el nombre de la mascota del jugador.
+const spanNamePetPlayer = document.getElementById('name-pet-player')
+// Span en el detalle del combate con las vidas del jugador.
+const spanLivesPetPlayer = document.getElementById('lives-pet-player')
+// Listado de ataques del jugador.
+const playerAttacks = document.getElementById('player-attacks')
+
+// Span en el detalle del combate con el nombre de la mascota del enemigo.
+const spanNamePetEnemy = document.getElementById('name-pet-enemy')
+// Span en el detalle del combate con las vidas del enemigo.
+const spanLivesPetEnemy = document.getElementById('lives-pet-enemy')
+// Listado de ataques del enemigo.
+const enemyAttacks = document.getElementById('enemy-attacks')
+
+// Espacio de mensaje con el resultado del combate.
+const scoreSection = document.getElementById('scores')
 
 /* ---- Variables globales ---- */
 
-// Mokepones
+// Array que contiene todos los mokepones.
 let mokepones = []
+// Array que contiene la generación de botones del combate
+let buttons = []
 
-// Ataques
-let playerAttack
-let enemyAttack
+// Variables globales que almacenan los ataques escogidos por el jugador y el enemigo.
+let enemyAttack = []
 
-// Almacenamiento de botones de selección de mascotas
+// Ataques que selecciona el jugador
+let playerAttack = []
+
+//
+let choiseAttackEnemy
+
+// Almacenamiento de botones de selección de mascotas luego de ejecutarse en "initGame".
 let inputHipodoge
 let inputCapipepo
 let inputRatigueya
 
-// Almacenamiento de botones de ataques de mascotas
+// Almacenamiento de botones de ataques de mascotas luego de ejecutarse en "showAttacks".
 let attacksMokepons
 let buttonFire
 let buttonWater
@@ -42,7 +72,7 @@ let petPlayer
 let playerLives = 3
 let enemyLives = 3
 
-//
+// Variable que almacena un string con la generación del boton de la mascota seleccionada en el string de mokepones. Se utiliza en "initGame".
 let mokeponOption
 
 /* ---- Clases ---- */
@@ -159,15 +189,18 @@ function selectPetPlayer() {
 // Selección aleatoria de mascota enemiga.
 function selectPetEnemy() {
     // Valor aleatorio para el valor del ataque enemigo.
+    // Toma la función de valores aleatorios "randomNumber" y pasa como parámetro la longitu del array "mokepones".
     let randomEnemy = randomNumber( 0 , mokepones.length - 1 )
     // Selección de la mascota del enemigo.
     spanNamePetEnemy.innerHTML = mokepones[randomEnemy].name
+    choiseAttackEnemy = mokepones[randomEnemy].attacks
+    secuenceAttacks()
 }
 // Mostrar ataques
 function showAttacks(attacks) {
     attacks.forEach(attack => {
         attacksMokepons = `
-        <button class="button-attacks" id="${attack.id}">${attack.name}</button>
+        <button class="button-attacks BAttacks" id="${attack.id}">${attack.name}</button>
         `
         attacksContainer.innerHTML += attacksMokepons
     })
@@ -176,35 +209,42 @@ function showAttacks(attacks) {
     buttonWater = document.getElementById('button-water')
     buttonEarth = document.getElementById('button-earth')
 
-    buttonFire.addEventListener('click', FireAttack)
-    buttonWater.addEventListener('click', WaterAttack)
-    buttonEarth.addEventListener('click', EarthAttack)
+    // Para no repetir ID del HTML, se utiliza una clase de CSS. Este arreglo trae todos los botones.
+    buttons = document.querySelectorAll('.BAttacks')
 }
-// Valores de ataque de mascotas.
-function FireAttack() {
-    playerAttack = 'Fuego';
-    randomEnemyAttack();
-}
-function WaterAttack() {
-    playerAttack = 'Agua';
-    randomEnemyAttack();
-}
-function EarthAttack() {
-    playerAttack = 'Tierra';
-    randomEnemyAttack();
+function secuenceAttacks() {
+    buttons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            if (e.target.textContent === 'Fuego') {
+                playerAttack.push('Fuego')
+                console.log(playerAttack)
+                button.style.background = '#112f58'
+            } else if (e.target.textContent === 'Agua') {
+                playerAttack.push('Agua')
+                console.log(playerAttack)
+                button.style.background = '#112f58'
+            } else if (e.target.textContent === 'Tierra') {
+                playerAttack.push('Tierra')
+                console.log(playerAttack)
+                button.style.background = '#112f58'
+            }
+            // Se coloca dentro del flujo para que no se elija primero el ataque enemigo y despues el jugador elija. Si se coloca por dentro del ForEach y por Fuera del IF se ejecuta 5 veces la misma.
+            randomEnemyAttack()
+        })
+    });
 }
 // Selección aleatoria de ataque enemigo
 function randomEnemyAttack() {
-    let randomAttack = randomNumber(1,3);
+    let randomAttack = randomNumber( 0 , choiseAttackEnemy.length - 1 )
     
-    if (randomAttack == 1) {
-        enemyAttack = 'Fuego'
-    } else if (randomAttack == 2) {
-        enemyAttack = 'Agua'
+    if (randomAttack == 0 || randomAttack == 1) {
+        enemyAttack.push('Fuego')
+    } else if (randomAttack == 3 || randomAttack == 4) {
+        enemyAttack.push('Agua')
     } else {
-        enemyAttack = 'Tierra'
+        enemyAttack.push('Tierra')
     }
-
+    console.log(enemyAttack)
     combat();
 }
 // Resultado de los combates entre mascotas.
